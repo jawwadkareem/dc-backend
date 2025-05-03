e3ac10dd316348ec3e2c0cfad44e45e1e84b6c9a
jawwadkareem <karimjawwad09@gmail.com>
2025-05-03 21:26:40 +0500

    Backend updated

--- api/tasks/[id].js
+++ api/tasks/[id].js
@@ -1,40 +1,40 @@
-const connectToDatabase = require('../../utils/db');
-const Task = require('../../models/Task');
+// const connectToDatabase = require('../../utils/db');
+// const Task = require('../../models/Task');
 
-module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+// module.exports = async (req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end();
+//   }
 
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ error: 'Method not allowed' });
+//   }
 
-  await connectToDatabase();
-  const { id } = req.query;
-  const { action } = req.body;
+//   await connectToDatabase();
+//   const { id } = req.query;
+//   const { action } = req.body;
 
-  try {
-    if (action === 'toggle') {
-      const task = await Task.findById(id);
-      if (!task) return res.status(404).json({ error: 'Task not found' });
+//   try {
+//     if (action === 'toggle') {
+//       const task = await Task.findById(id);
+//       if (!task) return res.status(404).json({ error: 'Task not found' });
 
-      task.completed = !task.completed;
-      await task.save();
-      return res.status(200).json(task);
-    }
+//       task.completed = !task.completed;
+//       await task.save();
+//       return res.status(200).json(task);
+//     }
 
-    if (action === 'delete') {
-      await Task.findByIdAndDelete(id);
-      return res.status(200).json({ message: 'Task deleted' });
-    }
+//     if (action === 'delete') {
+//       await Task.findByIdAndDelete(id);
+//       return res.status(200).json({ message: 'Task deleted' });
+//     }
 
-    return res.status(400).json({ error: 'Invalid action' });
-  } catch (err) {
-    return res.status(500).json({ error: 'Server error' });
-  }
-};
+//     return res.status(400).json({ error: 'Invalid action' });
+//   } catch (err) {
+//     return res.status(500).json({ error: 'Server error' });
+//   }
+// };
--- api/tasks/index.js
+++ api/tasks/index.js
@@ -1,9 +1,11 @@
+// index.js (in /api/tasks/index.js)
+
 const connectToDatabase = require('../../utils/db');
 const Task = require('../../models/Task');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
@@ -18,10 +20,34 @@ module.exports = async (req, res) => {
   }
 
   if (req.method === 'POST') {
-    const { title } = req.body;
-    const newTask = new Task({ title });
-    await newTask.save();
-    return res.status(201).json(newTask);
+    const { title, id, action } = req.body;
+
+    try {
+      if (action === 'toggle') {
+        const task = await Task.findById(id);
+        if (!task) return res.status(404).json({ error: 'Task not found' });
+
+        task.completed = !task.completed;
+        await task.save();
+        return res.status(200).json(task);
+      }
+
+      if (action === 'delete') {
+        await Task.findByIdAndDelete(id);
+        return res.status(200).json({ message: 'Task deleted' });
+      }
+
+      // Default action: Create
+      if (title) {
+        const newTask = new Task({ title });
+        await newTask.save();
+        return res.status(201).json(newTask);
+      }
+
+      return res.status(400).json({ error: 'Invalid request body' });
+    } catch (err) {
+      return res.status(500).json({ error: 'Server error' });
+    }
   }
 
   return res.status(405).end(); // Method Not Allowed
